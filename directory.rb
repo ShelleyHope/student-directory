7f549c25e29266aab2331e56bcdec0622d529ad5
shelleyhope <shelleyandkenny@mac.com>
2014-04-02 14:36:29 +0100

    user input

--- directory.rb
+++ directory.rb
@@ -1,26 +1,23 @@
-# let's put all the students into an array
-students = [
-{name: "Steve Musgrave", cohort: :march},
-{name: "Sroop Sunar", cohort: :march},
-{name: "Colin Marshall", cohort: :march},
-{name: "Josh Fail-Brown", cohort: :march},
-{name: "Louise Lai", cohort: :march},
-{name: "Robin Doble", cohort: :march},
-{name: "Alex Wong", cohort: :march},
-{name: "Scott Dimmock", cohort: :march},
-{name: "Muhanad Al-Rubaiee", cohort: :march},
-{name: "Shelley Hope", cohort: :march},
-# she's a particularly gifted student
-{name: "Will Hall", cohort: :march},
-{name: "Oliver Delevingne", cohort: :march},
-{name: "Nico", cohort: :march},
-{name: "Apostolis", cohort: :march},
-{name: "Stefania", cohort: :march},
-{name: "Robert Leon", cohort: :march},
-{name: "Emma Williams", cohort: :march},
-{name: "Joey Wolf", cohort: :march},
-{name: "Julie Walker", cohort: :march},
-]
+# let's ask the user to input student names into an array
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << {name: name, cohort: :march}
+		puts "Now we have #{students.length} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+# return the array of students
+	students
+end
+
 
 def print_header
 	puts "The students of my cohort at Maker's Academy"
@@ -38,6 +35,7 @@ def print_footer(student)
 end
 
 # nothing happens until we call the methods:
+students = input_students
 print_header
 print(students)
 print_footer(students)
