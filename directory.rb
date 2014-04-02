c81711488cceb7fa2d70ce825ed30ef8dd8848f7
shelleyhope <shelleyandkenny@mac.com>
2014-04-02 19:46:58 +0100

    exercise 5

--- directory.rb
+++ directory.rb
@@ -24,16 +24,24 @@ def print_header
 	puts "------------"
 end
 
+# def print_list(people)
+# 	i = 0
+# 	while (i < people.length)
+# 		puts "#{i + 1}. #{people[i][:name]} (#{people[i][:cohort]} cohort)"
+# 		i += 1
+# 		end
+# end
+
 def print_list(people)
 	i = 0
 		people.each do |person|
 		# if person[:name].chars.first == "A"
 		# if person[:name].length < 12
-		puts "#{i += 1} #{person[:name]} (#{person[:cohort]} cohort)"
+		puts "#{i += 1}. #{person[:name]} (#{person[:cohort]} cohort)"
 		end
 		# end
-	# end
 end
+
 # below is an alternative method using with_index to number each student
 # def print_list(people)
 # 	people.each_with_index do |person, index|
