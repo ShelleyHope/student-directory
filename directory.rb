8a98224434dbcd4457d2ed75bc8244eb8f99ea60
shelleyhope <shelleyandkenny@mac.com>
2014-04-02 12:44:34 +0100

    used each method to print

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 19
 # let's put all the students into an array
 students = [
 "Steve Musgrave",
@@ -25,24 +24,8 @@ students = [
 # and then print them
 puts "The students of my cohort at Maker's Academy"
 puts "-----------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
-puts students[12]
-puts students[13]
-puts students[14]
-puts students[15]
-puts students[16]
-puts students[17]
-puts students[18]
+students.each do |student|
+	puts student
+end
 # finally we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
