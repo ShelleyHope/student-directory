579d66b9d404edc1638756882ffca46bb7bf9c61
shelleyhope <shelleyandkenny@mac.com>
2014-04-02 12:35:24 +0100

    array now used

--- directory.rb
+++ directory.rb
@@ -1,26 +1,48 @@
 student_count = 19
-# first, we print the list of students
-puts "The students of my cohort at Makers Academy"
+# let's put all the students into an array
+students = [
+"Steve Musgrave",
+"Sroop Sunar",
+"Colin Marshall",
+"Josh Fail-Brown",
+"Louise Lai",
+"Robin Doble",
+"Alex Wong",
+"Scott Dimmock",
+"Muhanad Al-Rubaiee",
+"Shelley Hope",
+# she's a particularly gifted student
+"Will Hall",
+"Oliver Delevingne",
+"Nico",
+"Apostolis",
+"Stefania",
+"Robert Leon",
+"Emma Williams",
+"Joey Wolf",
+"Julie Walker"
+]
+# and then print them
+puts "The students of my cohort at Maker's Academy"
 puts "-----------------"
-puts "Steve Musgrave"
-puts "Sroop Sunar"
-puts "Colin Marshall"
-puts "Josh Fail-Brown"
-puts "Louise Lai"
-puts "Robin Doble"
-puts "Alex Wong"
-puts "Scott Dimmock"
-puts "Muhanad Al-Rubaiee"
-puts "Shelley Hope" 
-# a particularly gifted student
-puts "Will Hall"
-puts "Oliver Delevingne"
-puts "Nico"
-puts "Apostolis"
-puts "Stefania"
-puts "Robert Leon"
-puts "Emma Williams"
-puts "Joey Wolf"
-puts "Julie Walker"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
 # finally we print the total
 puts "Overall, we have #{student_count} great students"
