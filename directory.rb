602f02b2a9c34f554ff4d55f31bd84063e1f27a1
shelleyhope <shelleyandkenny@mac.com>
2014-04-03 16:57:41 +0100

    printed by cohort

--- directory.rb
+++ directory.rb
@@ -2,50 +2,53 @@
 
 
 
-
-def input_students
-	print "Please enter the name of the students\n"
-# create an empty array
-	students = []
-	# get the first name
-	name = gets.chomp
-	# while the name is not empty, repeat this code
-	while !name.empty? do
-		# add the student hash to the array
+# def input_students
+# 	print "Please enter the name of the students\n"
+# # create an empty array
+# 	students = []
+# 	# get the first name
+# 	name = gets.chomp
+# 	# while the name is not empty, repeat this code
+# 	while !name.empty? do
+# 		# add the student hash to the array
 		
-		puts "Please enter the cohort of the students"		
-		# array_feb=["Feb","feb","february","February"]
-		# array_march=["Mar","march","mar","March"]
-		# if array_feb.select {|cohort_entry| cohort_entry == cohort}.any? 
-		# 	cohort = :Feb
-		# elsif array_march.select {|cohort_entry| cohort_entry == cohort}.any? 
-		# 	cohort = :March
+# 		puts "Please enter the cohort of the students"		
+# 		# array_feb=["Feb","feb","february","February"]
+# 		# array_march=["Mar","march","mar","March"]
+# 		# if array_feb.select {|cohort_entry| cohort_entry == cohort}.any? 
+# 		# 	cohort = :Feb
+# 		# elsif array_march.select {|cohort_entry| cohort_entry == cohort}.any? 
+# 		# 	cohort = :March
 		
-		cohort = gets.chomp
-		if cohort.downcase.chars[0]=="m" then cohort = :March
-		end
+# 		cohort = gets.chomp
+# 		if cohort.downcase.chars[0]=="m" then cohort = :March
+# 		elsif cohort.downcase.chars[0]=="f" then cohort = :Feb
+# 		end
 	
-		puts "Please enter the age of the students"
-		age = gets.chomp
-		if age == "" then age = "18" 
-		else age
-		end
+# 		puts "Please enter the age of the students"
+# 		age = gets.chomp
+# 		if age == "" then age = "18" 
+# 		else age
+# 		end
 
-		puts "Please enter the country of birth of the students"
-		country_of_birth = gets.chomp
-		students << {name: name, age: age, country_of_birth: country_of_birth, cohort: cohort}
-		puts "Now we have #{students.length} students. Please enter next student details or hit return to finish."
-		# get another name from the user
-		name = gets.chomp
-	end
+# 		puts "Please enter the country of birth of the students"
+# 		country_of_birth = gets.chomp
+# 		students << {name: name, age: age, country_of_birth: country_of_birth, cohort: cohort}
+# 		puts "Now we have #{students.length} students. Please enter next student details or hit return to finish."
+# 		# get another name from the user
+# 		name = gets.chomp
+	# end
 # return the array of students
 
-	students
-end
+# 	students
+# end
 
-def inputname(name="Bob")
-	@name=name
-end
+
+
+
+# def inputname(name="Bob")
+# 	@name=name
+# end
 
 
 def print_header
@@ -66,7 +69,9 @@ def print_list(people)
 		people.each do |person|
 		# if person[:name].chars.first == "A"
 		# if person[:name].length < 12
-		print "#{i += 1}. #{person[:name].capitalize} \t\t#{person[:age]} \t\t#{person[:country_of_birth].capitalize} \t\t(#{person[:cohort]} Cohort)\n"
+		# print "#{i += 1}. #{person[:name].capitalize} \t\t#{person[:age]} 
+		# \t\t#{person[:country_of_birth].capitalize} \t\t(#{person[:cohort]} Cohort)\n"
+		printf "#{i += 1}. #{person[:name]} \t\t(#{person[:cohort]} Cohort)\n"
 		end
 		# end
 end
@@ -83,9 +88,31 @@ def print_footer(student)
 end
 
 # nothing happens until we call the methods:
-students = input_students
-print_header
-print_list(students)
-print_footer(students)
+# students = input_students
+#print_header
 
+
+def Feb_cohort(people)
+	return people.find_all {|person| person[:cohort] == :Feb}
+	# return Feb_array
+	end
+def	March_cohort(people)
+	return people.find_all {|person| person[:cohort] == :March}
+	# return Mar_array
+end
+
+
+students = [{:name => "Adam",:cohort => :Feb},{:name => "Anthony",:cohort => :Feb},{:name => "Peter",:cohort => :Feb},
+	{:name => "Robin",:cohort => :March},{:name => "Bethsheeba",:cohort => :March},{:name => "Marcus",:cohort => :March}]
+
+#print_footer(students)
 #puts students
+# print_list(students)
+
+Feb_array = Feb_cohort(students)
+Mar_array = March_cohort(students)
+print_list(Feb_array)
+print_list(Mar_array)
+
+# puts Feb_array
+# puts Mar_array
