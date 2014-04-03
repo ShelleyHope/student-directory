a48e651504bc3364d20d8e86714053451565943c
shelleyhope <shelleyandkenny@mac.com>
2014-04-03 13:29:33 +0100

    interactive cohort and conversion to symbol

--- directory.rb
+++ directory.rb
@@ -1,4 +1,8 @@
 # let's ask the user to input student names into an array
+
+
+
+
 def input_students
 	print "Please enter the name of the students\n"
 # create an empty array
@@ -8,11 +12,28 @@ def input_students
 	# while the name is not empty, repeat this code
 	while !name.empty? do
 		# add the student hash to the array
+		
+		puts "Please enter the cohort of the students"		
+		# array_feb=["Feb","feb","february","February"]
+		# array_march=["Mar","march","mar","March"]
+		# if array_feb.select {|cohort_entry| cohort_entry == cohort}.any? 
+		# 	cohort = :Feb
+		# elsif array_march.select {|cohort_entry| cohort_entry == cohort}.any? 
+		# 	cohort = :March
+		
+		cohort = gets.chomp
+		if cohort.downcase.chars[0]=="m" then cohort = :March
+		end
+	
 		puts "Please enter the age of the students"
 		age = gets.chomp
+		if age == "" then age = "18" 
+		else age
+		end
+
 		puts "Please enter the country of birth of the students"
 		country_of_birth = gets.chomp
-		students << {name: name, age: age, country_of_birth: country_of_birth, cohort: :march}
+		students << {name: name, age: age, country_of_birth: country_of_birth, cohort: cohort}
 		puts "Now we have #{students.length} students. Please enter next student details or hit return to finish."
 		# get another name from the user
 		name = gets.chomp
@@ -20,6 +41,10 @@ def input_students
 # return the array of students
 
 	students
+end
+
+def inputname(name="Bob")
+	@name=name
 end
 
 
@@ -41,7 +66,7 @@ def print_list(people)
 		people.each do |person|
 		# if person[:name].chars.first == "A"
 		# if person[:name].length < 12
-		print "#{i += 1}. #{person[:name].capitalize} \t\t#{person[:age]} \t\t#{person[:country_of_birth].capitalize} \t\t(#{person[:cohort].capitalize} Cohort)\n"
+		print "#{i += 1}. #{person[:name].capitalize} \t\t#{person[:age]} \t\t#{person[:country_of_birth].capitalize} \t\t(#{person[:cohort]} Cohort)\n"
 		end
 		# end
 end
@@ -62,3 +87,5 @@ students = input_students
 print_header
 print_list(students)
 print_footer(students)
+
+#puts students
